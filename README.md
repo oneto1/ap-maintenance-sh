225831908d77fd34b702c3019b15f2444c048575
oneto1 <644739768@qq.com>
2019-12-16 21:25:33 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-a simple daily Script
+# a simple daily script
+
+how to use it ?
+
+it is easy to run
+
+~~~
+sudo apt intall -y minicom
+
+#minicom    -C, --capturefile=FILE : start capturing to FILE
+sudo minicom -C ($filename)cap
+
+vi go.sh (modify your $filename)
+
+sh go.sh
+~~~
+enjoy it ! 
+
--- go.sh
+++ go.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-grep 'NY'  sedtest |sed 's/^.*NY//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/南院: \n/'
+filename=sedtest
 
-grep 'BJ'  sedtest |sed 's/^.*BJ//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/滨江: \n/'
+grep 'MD'  $filename  |sed 's/^.*MD//'   | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed "s/08-411.*//g"|  sed '2s/^/明德: \n/'
 
-grep 'QM'  sedtest |sed 's/^.*QM//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/亲民: \n/'
+grep 'NY'  $filename |sed 's/^.*NY//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/南院: \n/'
 
-grep 'MD'  sedtest |sed 's/^.*MD//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/明德: \n/'
+grep 'BJ'  $filename |sed 's/^.*BJ//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/滨江: \n/'
 
-grep 'XH'  sedtest |sed 's/^.*XH//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/学海: \n/'
+grep 'QM'  $filename |sed 's/^.*QM//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/亲民: \n/'
+
+# grep 'MD'  sedtest  |sed 's/^.*MD//'   | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed "s/08-411.*/\t/g"|  sed '2s/^/明德: \n/'
+
+grep 'XH'  $filename |sed 's/^.*XH//'  | sed 's/ap.*//'  | sed 's/\(.*\)\(.\{6\}\)/\1/g' |sed '/^[A-Z]/d' |sed 's/ //' |sed 's/\(.*\)\(.\{7\}\)/\1/' | sed '$!N;s/\n/\t/' |  sed '$!N;s/\n/\t/'| sed '1s/^/学海: \n/'
 
